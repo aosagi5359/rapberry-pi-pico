65b427e76b15c99babc39fe4a781c1d6a05635c9
aosagi5359 <115853391+aosagi5359@users.noreply.github.com>
2023-12-20 15:30:58 +0800

    Update prob04.py

--- prob04.py
+++ prob04.py
@@ -5,8 +5,7 @@ import _thread as thread
 # Define GPIO pins
 red1, yellow1, green1 = 2, 3, 4
 red2, yellow2, green2 = 6, 7, 8
-button_pin1 = 15
-button_pin2 = 14
+button_direction_pin = 14
 
 # Initialize GPIO pins
 red1_led = machine.Pin(red1, machine.Pin.OUT)
@@ -15,72 +14,60 @@ green1_led = machine.Pin(green1, machine.Pin.OUT)
 red2_led = machine.Pin(red2, machine.Pin.OUT)
 yellow2_led = machine.Pin(yellow2, machine.Pin.OUT)
 green2_led = machine.Pin(green2, machine.Pin.OUT)
-button1 = machine.Pin(button_pin1, machine.Pin.IN, machine.Pin.PULL_UP)
-button2 = machine.Pin(button_pin2, machine.Pin.IN, machine.Pin.PULL_UP)
+button_direction = machine.Pin(button_direction_pin, machine.Pin.IN, machine.Pin.PULL_UP)
 
-# Variable to indicate the current LED direction
-current_direction = 'horizontal'
+# Variable to indicate the current direction
+current_direction = 1  # 1: Red-Horizontal, Green-Vertical; 2: Green-Horizontal, Red-Vertical
 
-def set_horizontal_direction():
+def switch_direction():
     global current_direction
-    current_direction = 'horizontal'
-    print("Direction: Horizontal")
 
-def set_vertical_direction():
+    while True:
+        if current_direction == 1:
+            red1_led.value(1)
+            green2_led.value(1)
+            time.sleep(0.5)
+            red1_led.value(0)
+            green2_led.value(0)
+            time.sleep(0.5)
+
+            green1_led.value(1)
+            red2_led.value(1)
+            time.sleep(0.5)
+            green1_led.value(0)
+            red2_led.value(0)
+            time.sleep(0.5)
+        elif current_direction == 2:
+            green1_led.value(1)
+            red2_led.value(1)
+            time.sleep(0.5)
+            green1_led.value(0)
+            red2_led.value(0)
+            time.sleep(0.5)
+
+            red1_led.value(1)
+            green2_led.value(1)
+            time.sleep(0.5)
+            red1_led.value(0)
+            green2_led.value(0)
+            time.sleep(0.5)
+
+# Function to handle direction button press
+def direction_button_pressed(pin):
     global current_direction
-    current_direction = 'vertical'
-    print("Direction: Vertical")
 
-# Function to handle button1 press
-def button1_pressed(pin):
-    set_horizontal_direction() if current_direction == 'vertical' else set_vertical_direction()
+    if current_direction == 1:
+        current_direction = 2
+    elif current_direction == 2:
+        current_direction = 1
 
-# Function to handle button2 press
-def button2_pressed(pin):
-    set_horizontal_direction() if current_direction == 'vertical' else set_vertical_direction()
+# Set up direction button interrupt
+button_direction.irq(trigger=machine.Pin.IRQ_FALLING, handler=direction_button_pressed)
 
-# Set up button1 interrupt
-button1.irq(trigger=machine.Pin.IRQ_FALLING, handler=button1_pressed)
-
-# Set up button2 interrupt
-button2.irq(trigger=machine.Pin.IRQ_FALLING, handler=button2_pressed)
-
-# LED operations
-def horizontal_direction():
-    while current_direction == 'horizontal':
-        green1_led.value(1)
-        red2_led.value(1)
-        time.sleep(0.5)
-        green1_led.value(0)
-        red2_led.value(0)
-        time.sleep(0.5)
-
-        red1_led.value(1)
-        green2_led.value(1)
-        time.sleep(0.5)
-        red1_led.value(0)
-        green2_led.value(0)
-        time.sleep(0.5)
-
-def vertical_direction():
-    while current_direction == 'vertical':
-        red1_led.value(1)
-        green2_led.value(1)
-        time.sleep(0.5)
-        red1_led.value(0)
-        green2_led.value(0)
-        time.sleep(0.5)
-
-        green1_led.value(1)
-        red2_led.value(1)
-        time.sleep(0.5)
-        green1_led.value(0)
-        red2_led.value(0)
-        time.sleep(0.5)
+# Start the direction switching thread
+thread.start_new_thread(switch_direction, ())
 
 # Main loop
 while True:
-    if current_direction == 'horizontal':
-        horizontal_direction()
-    elif current_direction == 'vertical':
-        vertical_direction()
+    # Your main code logic here (based on the current direction)
+    pass
